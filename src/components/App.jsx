c34a34ee1995288f2e5992419f905c5873b48ffa
andrewdark <samsonov.a@ukr.net>
2023-10-09 14:56:29 +0300

    add some structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,29 @@
 import React, {Component} from "react";
 
 class App extends Component{
+  appStyles = {
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    gridGap: '16px',
+    paddingBottom: '24px',
+  };
+
+  state = {
+    images: [],
+    searchQuery: '',
+    per_page: 12,
+    page: 1,
+    isLoading: false,
+    isLoadMore: false,
+    isModalShow: false,
+    pict: { url: null, alt: 'no image' },
+    error: null,
+  };
 
   render() {
     return (
       <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101'
-        }}
+        style={this.appStyles}
       >
         React homework template
       </div>
